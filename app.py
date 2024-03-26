aea73b05812883c38e10fc49d03d607023d2cc0e
jackforgothispassword <136520620+jackforgothispassword@users.noreply.github.com>
2024-03-26 10:30:17 -0700

    add target_audience param

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
+# import libraries
 from flask import Flask, render_template, request
 import os
 from openai import OpenAI
 
+#start flask app and set OpenAI API Key via .env
 app = Flask(__name__)
 client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
+#Generate video ideas using prompt
 def generate_video_ideas(niche):
-    prompt = f"Using knowledge of YouTube and successful videos on the platform, generate 10 video content ideas for a YouTube channel about {niche}. Then add a new section with 5 example YouTubers to research for the proposed niche."
+    prompt = f"Using knowledge of YouTube and successful videos on the platform, generate 10 video content ideas for a YouTube channel about the {niche} niche, for the target audience of {target_audience}. Then add a new section with 5 example YouTubers to research for the proposed niche."
 
+    # Specify model
     completion = client.completions.create(
         model="gpt-3.5-turbo-instruct",
         prompt=prompt,
@@ -21,8 +25,9 @@ def generate_video_ideas(niche):
 def home():
     if request.method == 'POST':
         niche = request.form.get('niche')
-        video_ideas = generate_video_ideas(niche)
-        return render_template('index.html', video_ideas=video_ideas, niche=niche)
+        target_audience = request.form.get('target_audience')
+        video_ideas = generate_video_ideas(niche, target_audience)
+        return render_template('index.html', video_ideas=video_ideas, niche=niche, target_audience=target_audience)
     else:
         return render_template('index.html', video_ideas=None)
 
